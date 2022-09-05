03b920ed0e2b5ba37ac103441c3551e00b4c14ff
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-09-05 22:11:11 +0900

    2022-08-08~2022-09-05

--- web-2022-08/src/main/webapp/mission/score_control.js
+++ web-2022-08/src/main/webapp/mission/score_control.js
@@ -1,6 +1,5 @@
 /**
- * 성적 CRUD
- * 작성일: 2022-08-30
+ * 
  */
 let data = [
 	{'stdId' : '1', 'subject' : '국어', 'score' : '100'},
@@ -11,85 +10,85 @@ let data = [
 	{'stdId' : '4', 'subject' : '역사', 'score' : '100'}
 ];
 
-let template;     // 배열 data의 인덱스를 보기 좋게 리스트에 띄우는 작업을 쉽게 하기 위해 선언.
-let m_index;      // 배열의 위치값을 모든 함수에서 사용하기 위해 선언. 전역형으로 선언해두면 함수 끝나도 사라지지 않음.
+let m_index;   // 배열의 위치값을 모든 함수에서 사용하기 위해 선언. 전역형으로 선언해두면 함수가 끝나도 사라지지 않음.
+let template;  // 배열 data의 맵구조를 보기 좋게 리스트에 띄우는 작업을 쉽게 하기 위해 선언.
 let frm = document.frm_score;  // form에 있는 태그나 태그 속성들을 가져오기 쉽게 객체로 선언.
+List();        // List 함수는 Hoisting되기 때문에 List 함수보다 위에 적어도 상관없다.
 
-List();           // List 함수는 Hoisting되기 때문에 List 함수보다 위에 적어도 상관없다.
-
-function List(){  // 
+function List(){
 	let items = document.querySelector('#items');
-	let avg = document.querySelector('#avg');
-	let tot = 0;  // 평균 계산을 위한 합계 선언.
-	for(var v in data){        // 배열 data의 인덱스를 보기 좋게 리스트에 차례대로 띄움.
-							   // 리스트의 인덱스를 클릭하면 View 함수 실행.
-		template = `<div class = 'item' onclick = 'View("${v}")'>
-						<span class = 'stdId'>${data[v].stdId}</span>
-						<span class = 'subject'>${data[v].subject}</span>
-						<span class = 'score'>${data[v].score}</span>
+	items.innerHTML = '';
+	let tot = 0;          // score의 value 값들의 평균을 구하기 위해 합계를 먼저 선언.
+	for(index in data){   // 배열 data의 맵구조를 보기 좋게 리스트에 차례대로 띄움.
+						  // 리스트를 클릭하면 해당 맵구조를 View 함수에 넣어서 실행.
+		v = data[index];  // ${0}, ${1}, ... -> view('0'), view('1'), ...
+		template = `<div class='item' onclick='view("${index}")'>
+						<span class='stdId'>${v.stdId}</span>
+						<span class='subjectject'>${v.subject}</span>
+						<span class='score'>${v.score}</span>
 					</div>`;
 		items.innerHTML += template;
-		tot += Number(data[v].score);
+		tot += Number(v.score);
 	}
-	avg.innerHTML = '평균: ' + (tot/data.length).toFixed(2);
+	items.innerHTML += '평균: ' + (tot/data.length).toFixed(2);
 }
 
 /* 조회 */
-function View(v){
-	m_index = v;
-	frm.stdId.value = data[v].stdId;
-	frm.subject.value = data[v].subject;
-	frm.score.value = data[v].score;
+function View(index){
+	m_index = index;  // form에 표시되는 인덱스가 m_index. m_index에 클릭한 리스트에 표시되는 인덱스를 매개변수로 받아서 넣어라.
+	frm.stdId.value = data[index].stdId;      // 선택한 리스트의 stdId키값의 value값을 form의 텍스트 박스에 넣어라.
+	frm.subject.value = data[index].subject;  // 선택한 리스트의 subject키값의 value값을 form의 텍스트 박스에 넣어라.
+	frm.score.value = data[index].score;      // 선택한 리스트의 score키값의 value값을 form의 텍스트 박스에 넣어라.
 }
 
 /* 입력 */
 function Add(frm){
 	let v;
-	v = {'stdId'   : frm.stdId.value,
-		 'subject' : frm.subject.value,
-		 'score'   : frm.score.value
+	v = {  // form에 입력된 value값을 맵구조로 만든다.
+		'stdId'    : frm.stdId.value,
+		'subject'   : frm.subject.value,
+		'score' : frm.score.value
 		};
 	
+	let items = document.querySelector('#items');
+	
 	ChkId  = data.findIndex( d=>d.stdId==frm.stdId.value );
-	Chksub = data.findIndex( d=>d.subject==frm.subject.value );
-	if(index>=0 && index2>=0){		// 학번+과목 중복체크
+	ChkSub = data.findIndex( d=>d.subject==frm.subject.value );
+	
+	// 학번+과목 중복체크.
+	if(ChkId>=0 && ChkSub>=0){
 		alert('해당 학번에 이미 해당 과목의 점수가 입력되어 있습니다.');
 		frm.stdId.focus();
 		return;
 	}
-	if(frm.stdId.value==''){		// 학번 입력 여부 체크
-		alert('학번을 입력해 주세요.');
+	// 학번 입력 여부 체크
+	if(frm.stdId.value==''){
+		alert('학번을 확인해 주세요.')
 		frm.stdId.focus();
 		return;
 	}
-	if(frm.subject.value==''){		// 과목 입력 여부 체크
-		alert('과목을 입력해 주세요.');
+	// 과목 입력 여부와 숫자인지 체크
+	if(frm.subject.value==''){
+		alert('과목을 확인해 주세요.');
 		frm.subject.focus();
 		return;
 	}
-	if(frm.score.value==''){		// 성적 입력 여부 체크
-		alert('성적을 입력해 주세요.');
+	// 성적 입력 여부 체크
+	if(frm.score.value=='' || isNaN(frm.score.value)){  // 입력하지 않았거나 문자라면
+		alert('성적을 확인해 주세요')
 		frm.score.focus();
 		return;
 	}
 	
-	data.push(v);  // 배열 뒤에 추가
-	template = 
-		`
-		<div class = 'item' onclick = 'view("${v.stdId}")'>
-			<span class = 'stdId'>${v.stdId}</span>
-			<span class = 'subject'>${v.subject}</span>
-			<span class = 'score'>${v.score}</span>
-		</div>
-		`
-	items.innerHTML += template;  // 목록 뒤에 추가
-	a();  // 평균 계산 함수 실행
+	data.push(v);  // 배열뒤에 추가
+	List();        // 이미 뿌려진 화면 지우고 다시 뿌림. 별로 좋은 방법은 아님.
 }
 
 /* 수정 */
-function update(frm){
-	let index = data.findIndex( d=>d.stdId==frm.stdId.value);
-	if(index>=0){
+function Update(frm){  // 수정을 누르는 순간 입력 상자 안의 내용이 frm이 된다.
+	let index = m_index;
+	
+	if(index>=0){      // 수정할 데이터가 있는경우. 바꿀 수 있는 이유가 m_index가 있어서.
 		data[index].stdId = frm.stdId.value;
 		data[index].subject = frm.subject.value;
 		data[index].score = frm.score.value;
@@ -98,25 +97,25 @@ function update(frm){
 	}else{
 		alert('수정할 데이터가 없습니다.');
 	}
-	a();  // 평균 계산 함수 실행
 }
 
-/* 삭제 */
+/* 데이터 삭제 */
 function deleteFunc(frm){
-	let index = data.findIndex( d=>d.stdId==frm.stdId.value);
+	let index = m_index;
+	
 	if(index>=0){
-		data.splice(index,1);
+		data.splice(index,1);  // 중요 포인트
 		List();
 		frm.stdId.value='';
 		frm.subject.value='';
 		frm.score.value='';
+		alert('데이터가 삭제되었습니다.');
 	}else{
-		alert('삭제할 데이터를 찾지 못했습니다.');
+		alert('삭제할 자료를 찾지 못했습니다.');
 	}
-	a();  // 평균 계산 함수 실행
 }
 
-/* 평균 계산 내 코드
+/* 평균 계산 type2
 function a(){
 	let result = document.querySelector('#result');  // 평균 출력 위치
 	let scoreData = data.map(v=>v.score);            // 배열 data에서 키값이 score인 밸류값들을 모아서 만든 배열
--- web-2022-08/src/main/webapp/mission/score_control_teacher.js
+++ web-2022-08/src/main/webapp/mission/score_control_teacher.js
@@ -1,110 +1,115 @@
 /**
- * 
+ * 성
  */
-let data = [
-	{'id' : '1', 'sub' : '국어', 'score' : '100'},
-	{'id' : '1', 'sub' : '영어', 'score' : '90'},
-	{'id' : '2', 'sub' : '역사', 'score' : '95'},
+ let m_index; // 배열의 위치값을 모든 function에서 사용하기 위해.
+ 
+ let data=[
+    {'id' : 'a001', 'subject' : '국어', 'score': 99},
+    {'id' : 'a002', 'subject' : '수학', 'score': 89},
+    {'id' : 'a003', 'subject' : '바른생활', 'score': 79},
+    {'id' : 'a004', 'subject' : '세계사', 'score': 69},
+    {'id' : 'a001', 'subject' : '한국사', 'score': 79},
+    {'id' : 'a001', 'subject' : '산수', 'score': 89},
+    {'id' : 'a002', 'subject' : '영어', 'score': 95},
+    {'id' : 'a002', 'subject' : '독어', 'score': 94},
+    {'id' : 'a003', 'subject' : '일어', 'score': 93}
 ];
-
-let m_index;  // 배열의 인덱스 위치값을 모든 function에서 사용하기 위해.
-let frm = document.frm_score;
 let template;
-List();
+list();
 
-function List(){
-	let items = document.querySelector('#items');
-	items.innerHTML = '';
-	let tot = 0;          // score 합계
-	for(index in data){
-		v = data[index];  // ${0}, ${1}, ... -> view('0'), view('1'), ...
-		template = 
-			`
-			<div class='item' onclick='view("${index}")'>
-				<span class='id'>${v.id}</span>
-				<span class='subject'>${v.sub}</span>
-				<span class='score'>${v.score}</span>
-			</div>
-			`;
-		items.innerHTML += template;
-		tot += Number(v.score);
-	}
-	items.innerHTML += '평 균 : ' + (tot/data.length).toFixed(1);
+function list(){
+    let items = document.querySelector('#items');
+    items.innerHTML = '';
+    let tot=0;
+    for(index in data){
+        v = data[index];
+        template  =
+             `
+            <div class='item' onclick='view("${index}")'>
+              <span class='id'>${v.id}</span>
+              <span class='subject'>${v.subject}</span>
+              <span class='score'>${v.score}</span>
+            </div> 
+            `
+        items.innerHTML += (template);
+        tot += Number(v.score);
+    }
+    items.innerHTML += '평 균 : ' + (tot/data.length).toFixed(1);
 }
 
-function View(index){
-	m_index = index;  // input에 있는게 m_index. 전역형.
-	let frm = document.frm_score;
-	frm.id.value = data[index].id;
-	frm.sub.value = data[index].sub;
-	frm.score.value = data[index].score;
+function view(index){
+    m_index = index;
+    let frm = document.frm_score;
+    frm.id.value = data[index].id;
+    frm.subject.value = data[index].subject;
+    frm.score.value = data[index].score;
+} 
+//데이터 추가 
+
+function add(frm){
+    let v;
+    let items = document.querySelector('#items');
+    let status = document.querySelector('.status');
+    
+    // 학번 입력 여부 체크
+    if(frm.id.value==''){
+        status.innerHTML = ('학번을 확인해 주세요');
+        frm.id.focus();
+        return;
+    }   
+   
+   
+    // 과목 입력 여부 체크
+    if(frm.subject.value==''){
+        status.innerHTML = ('과목을 확인해 주세요');
+        frm.subject.focus();
+        return;
+    }   
+   
+    // 성적 입력 여부 체크
+    if(frm.score.value == '' || isNaN(frm.score.value)){
+        status.innerHTML = ('성적을 확인해 주세요');
+        frm.score.focus();
+        return;
+    }
+
+    // form 값을 {}로 만들어 data에 push
+    v = {
+        'id': frm.id.value, 'subject': frm.subject.value,
+        'score': frm.score.value
+        };
+    data.push(v);// 배열뒤에 추가
+
+    list();
+} 
+ 
+function update(frm){
+    let index = m_index;
+    let status = document.querySelector('.status');
+    
+    if(index>=0){ // 수정할 데이터가 있는 경우
+        data[index].id = frm.id.value;
+        data[index].subject = frm.subject.value;
+        data[index].score =frm.score.value;
+        list();
+        status.innerHTML = ('데이터가 수정되었습니다.');    
+    }else{
+        status.innerHTML = ('수정할 데이터가 없습니다.');
+    }
 }
 
-/* 데이터 추가 */
-function Add(frm){
-	let v;
-	let items = document.querySelector('#items');
-	let status = document.querySelector('.status');
-	
-	// 학번 입력 여부 체크
-	if(frm.id.value==''){
-		status.innerHTML = '학번을 확인해 주세요.';
-		frm.id.focus();
-		return;
-	}
-	// 과목 입력 여부 체크
-	if(frm.sub.value==''){
-		status.innerHTML = '과목을 확인해 주세요.';
-		frm.sub.focus();
-		return;
-	}
-	// 성적 입력 여부 체크
-	if(frm.score.value=='' || isNaN(frm.score.value)){  // 입력하지 않았거나 문자라면
-		status.innerHTML = '성적을 확인해 주세요';
-		frm.score.focus();
-		return;
-	}
-	// form 값을 {}로 만들어 data에 push
-	v = {
-		'id'    : frm.id.value,
-		'sub'   : frm.sub.value,
-		'score' : frm.score.value
-		};
-	// 배열뒤에 추가
-	data.push(v);
-	
-	List();  // 이미 뿌려진 화면 지우고 다시 뿌림. 별로 좋은 방법은 아님.
-}
-
-/* 데이터 수정 */
-function Update(frm){  // 수정을 누르는 순간 입력 상자 안의 내용이 frm이 된다.
-	let index = m_index;
-	let status = document.querySelector('.status');
-	
-	if(index>=0){      // 수정할 데이터가 있는경우. 바꿀 수 있는 이유가 m_index가 있어서.
-		data[index].id = frm.id.value;
-		data[index].sub = frm.sub.value;
-		data[index].score = frm.score.value;
-		List();
-		status.innerHTML = '데이터가 수정되었습니다.';
-	}else{
-		status.innerHTML = '수정할 데이터가 없습니다.';
-	}
-}
-
-/* 데이터 삭제 */
 function deleteFunc(frm){
-	let index = m_index;
-	let status = document.querySelector('.status');
-	
-	if(index>=0){
-		data.splice(index,1);  // 중요 포인트
-		List();
-		frm.id.value='';
-		frm.sub.value='';
-		frm.score.value='';
-		status.innerHTML = '데이터가 삭제되었습니다.';
-	}else{
-		status.innerHTML = '삭제할 자료를 찾지 못했습니다.';
-	}
+    let index = m_index;
+    let status = document.querySelector('.status');
+    
+    if(index>=0){
+       data.splice(index,1);
+       list();
+       frm.id.value='';
+       frm.subject.value='';
+       frm.score.value='';
+       status.innerHTML = '데이터가 삭제되었습니다.';
+    }else{
+        status.innerHTML = ('삭제할 자료를 찾지 못했습니다.');
+    }
 }
