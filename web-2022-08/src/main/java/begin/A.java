e6622205c6a534fca5cbe271b2daa153811380f1
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-09-26 22:28:39 +0900

    2022-08-08~2022-09-26

---
+++ web-2022-08/src/main/java/begin/A.java
@@ -0,0 +1,94 @@
+package begin;
+
+public class A {
+	public static void main(String[] abc) {
+		
+//		System.out.println("강수형");
+//		System.out.println("서울대입구");
+//		System.out.println(10 + 10);
+		
+//		byte b = '0';
+//		System.out.println((char)b+(char)b);
+//		System.out.println((char)b+1);
+//		System.out.println((char)b);
+//		System.out.println(b);
+//		System.out.println("");
+		
+//		char t = '0';
+//		System.out.println((char)t+(char)t);
+//		System.out.println((char)t+1);
+//		System.out.println(t+1);
+//		System.out.println((char)t);
+//		System.out.println(t);
+//		t += 1;
+//		System.out.println(t);
+//		System.out.println("");
+		
+		/* 변수형 */
+//		boolean b1 = false;
+//		boolean b2 = 10<20;
+//		System.out.println("b1=" + b1);
+//		System.out.println("b2=" + b2);
+		
+		/* 문자형 */
+//		char c1 = '한';
+//		c1 += 1;
+//		System.out.println("c1=" + c1);
+//		char c2 = '1';
+//		c2 += 1;
+//		System.out.println("c2=" + c2);
+		
+		/* long */
+//		long l1 = 10L;
+		
+		/* float */
+//		float f1 = (float)3.14; // 3.14f로 인식
+//		float f2 = 3.14f;       // 3.14f로 인식
+//		float f3 = 3.14;        // 오류
+//		float f4 = 3;           // 3.0f로 인식
+		
+		/* 수동 형변환(Casting) */
+//		double pi = 3.14;
+//		int a  = (int)pi; // 3으로 인식
+		                  // byte, short, char는 정수와 연산하면 정수로
+		
+		/* 정수형 연산 */
+//		int test1 = 10;
+//		int test2 = 4;
+//		System.out.println(test1 / test2); // 정수끼리 연산은 정수
+		
+//		double test3 = 10;
+//		double test4 = 4;
+//		System.out.println(test3 / test4); // 실수끼리 연산은 실수
+		
+//		int test5 = 10;
+//		double test6 = 4;
+//		System.out.println(test5 / test6);
+		
+//		int test7 = 10;
+//		double test8 = 4.0;
+//		System.out.println(test7 / test8);
+		
+		char c1 = '0';
+		System.out.println(c1 + 1);
+		c1 += 1;
+		System.out.println(c1);
+		System.out.println((char)49);
+		System.out.println((char)13);
+		System.out.println((char)49);
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ web-2022-08/src/main/java/begin/StringEx.java
@@ -0,0 +1,89 @@
+package begin;
+
+import java.io.UnsupportedEncodingException;
+
+public class StringEx {
+	public static void main(String[] args) {
+//		String s1 = "홍길동";
+//		String s2 = "홍길동";
+//		System.out.println(s1==s2);
+		/* "홍길동" 이라는 값을 비교하는게 아니라
+		 * heap memory에 "홍길동"을 가리키는
+		 * 주소가 같은지 비교 한다.
+		 */
+//		System.out.println(s1.equals(s2));
+		/* "홍길동" 이라는 값(문자열)을 비교하려면
+		 * equals를 사용한다.
+		 */
+		
+//		String s3 = new String("일지매");
+//		String s4 = new String("일지매");
+//		System.out.println(s3==s4);
+//		System.out.println(s3.equals(s4));
+		
+		
+		
+//		System.out.println("----------");
+//		String jumin = "123456-1";
+		
+//		char c = jumin.charAt(7);
+//		System.out.println(c);
+		
+//		int r = Character.getNumericValue(c);
+//		String str = null;
+//		if(r%2==0) str = "여자";
+//		else       str = "남자";
+//		System.out.println("c=" + c);
+//		System.out.println("성별=" + str);
+		
+//		if(c=='1' || c=='3') str = "진짜 남자 !!";
+//		else                 str = "진짜 여자 !!";
+//		System.out.println(str);
+		
+		/* String pn='123R456'인 경우 4번째 값은
+		 * R:red, B:blue, G:green 색을 나타내고 있다.
+		 * 위의 pn을 확인하여 어떤색의 제품인지 출력
+		 */
+		String pn = "123R456";
+		
+		int pnLength = pn.length();
+		String pnStr = null;
+		
+		for(int i=0; i<pnLength; i++) {
+			char pnFindColor = pn.charAt(i);
+			if(pnFindColor=='R')      pnStr = "빨간색 제품 입니다.";
+			else if(pnFindColor=='G') pnStr = "초록색 제품 입니다.";
+			else if(pnFindColor=='B') pnStr = "파란색 제품 입니다.";
+		}
+		
+		System.out.println(pnStr);
+		
+		/* getBytes */
+		String strGet = "abc 123 가나다";
+		try {
+			byte[] euc = strGet.getBytes("euc-kr");
+			byte[] ksc = strGet.getBytes("ksc5601");
+			byte[] iso = strGet.getBytes("iso8859-1");
+			byte[] utf = strGet.getBytes("utf-8");
+			
+			System.out.println("----------");
+			System.out.println("euc-kr : " + new String(euc));
+			System.out.println("ksc : " + new String(ksc));
+			System.out.println("iso : " + new String(iso));
+			System.out.println("utf : " + new String(utf));
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
