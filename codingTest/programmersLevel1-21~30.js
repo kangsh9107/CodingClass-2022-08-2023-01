aa33339fe0954b7bf101ad594386404c72ddb54d
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-09-30 20:00:42 +0900

    practice

--- codingTest/programmersLevel1-21~30.js
+++ codingTest/programmersLevel1-21~30.js
@@ -143,27 +143,52 @@
 // }
 // console.log(solution(6, 3));
 
+// 다른 사람 풀이
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => { // 입력받은 숫자를 'data'에 받는다
+//     const n = data.split(" ");     // data(입력받은 숫자)를 배열로 변경
+//     const a = Number(n[0]), b = Number(n[1]); // a는 한 줄에 대한 별의 갯수, b는 몇 줄 출력
+//     for(let i=0; i<b; i++){
+//         let str = "";
+//         for(let j=0; j<a; j++){
+//             str = str + "*"
+//         } 
+//     console.log(str)
+//     }
+// });
+
 // 30. 최대공약수와 최소공배수 //////////////////////////////////////////////////
+// function solution(n, m) {
+//     let answer = [];
+//     let nYak = [];
+//     let mYak = [];
+//     let yak = [];
+//     let nGong = [];
+//     let mGong = [];
+//     let gong = [];
+    
+//     for(let i=1; i<=n; i++) {
+//         if(n%i===0) nYak.push(i);
+//     }
+//     for(let k=1; k<=m; k++) {
+//         if(m%k===0) mYak.push(k);
+//     }
+//     for(v of nYak) {
+//         if(mYak.indexOf(v)!==-1) yak.push(v);
+//     }
+
+//     console.log(nYak,mYak);
+//     return answer = [yak.pop(), gong.shift()];
+// }
+// console.log(solution(3, 12));
+
 function solution(n, m) {
     let answer = [];
-    let nYak = [];
-    let mYak = [];
-    let yak = [];
     let nGong = [];
     let mGong = [];
-    let gong = [];
-    
-    for(let i=1; i<=n; i++) {
-        if(n%i===0) nYak.push(i);
-    }
-    for(let k=1; k<=m; k++) {
-        if(m%k===0) mYak.push(k);
-    }
-    for(v of nYak) {
-        if(mYak.indexOf(v)!==-1) yak.push(v);
-    }
 
-    console.log(nYak,mYak);
-    return answer = [yak.pop(), gong.shift()];
+    
+
+    return answer;
 }
 console.log(solution(3, 12));
