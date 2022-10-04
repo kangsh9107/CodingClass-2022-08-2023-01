78fef36a09bedf551442800e151ec2f45fbb3502
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-10-04 23:45:51 +0900

    2022-08-08~2022-10-04

--- web-2022-08/src/main/java/standard/Practice.java
+++ web-2022-08/src/main/java/standard/Practice.java
@@ -1,25 +1,7 @@
 package standard;
 
 public class Practice {
-	
-	public static void main(String[] args) {
-//		System.out.println("    *");
-//		System.out.println("   ***");
-//		System.out.println("  *****");
-//		System.out.println(" *******");
-//		System.out.println("*********");
-		
-		String str1 = "";
-		String str2 = "";
-		for(int i=0; i<10; i++) {
-			if(i==0)      str1 = "    ";
-			else if(i==2) str1 = "   ";
-			else if(i==4) str1 = "  ";
-			else if(i==6) str1 = " ";
-			else          str1 = "";
-			str2 += "*";
-			if(i%2==0) System.out.println(str1 + str2);
-		}
-	}
-	
+    public static void main(String[] args) {
+    	
+    }
 }
