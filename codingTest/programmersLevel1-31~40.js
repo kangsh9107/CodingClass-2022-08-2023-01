1845990f11575edb9c50ef51b5a3652a15b75ed2
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-10-04 18:30:47 +0900

    practice

--- codingTest/programmersLevel1-31~40.js
+++ codingTest/programmersLevel1-31~40.js
@@ -1,9 +1,8 @@
 // 31. 같은 숫자는 싫어
 function solution(arr) {
     let answer = [];
-    for(let i=0; i<arr.length; i++) {
-        if(arr[i]===arr[i+1]) answer.push(arr[i]);
-    }
+
+    answer = arr.filter((v, i) => arr.indexOf(v) === i);
 
     return answer;
 }
