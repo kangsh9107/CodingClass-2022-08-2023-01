277009674581186e54ee259996f645b64d0650bf
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-10-16 03:10:45 +0900

    2022-08-08~2022-10-16

--- web-2022-08/src/main/java/standard/P221014.java
+++ web-2022-08/src/main/java/standard/P221014.java
@@ -1,16 +1,43 @@
 package standard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 public class P221014 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		/*
 		String s = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
 		String test = "hipwg@naver.com";
 		boolean b = Pattern.matches(s, test);
 		System.out.print(b);
+		*/
 		
-		List<Integer> li = new ArrayList<>();
+		/*
+		int N = 18;
+		int x = 3;
+		int y = 5;
+		int ans = 0;
+
+		for(int i=N/5; i>=0; i--) {
+			y = 5 * i;
+			for(int j=0; j<=N/3; j++) {
+				x = 3 * j;
+				if(N == x+y) {
+					ans = i + j;
+					System.out.println(ans);
+					break;
+				}
+			}
+			if(N == x+y) break;
+		}
+		if(ans == 0) System.out.print(-1);
+		*/
+
+		
 	}
 }
---
+++ web-2022-08/src/main/java/standard/P221015.java
@@ -0,0 +1,89 @@
+package standard;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P221015 {
+
+	public static void main(String[] args) throws IOException {
+		
+		/*
+		int N = 3;
+		String[] arr = {"71","79","37","1"};
+		int cnt = 0;
+		int ans = 0;
+
+		for(int i=0; i<N; i++) {
+			cnt = 0;
+			int temp = Integer.parseInt(arr[i]);
+			
+			for(int j=2; j<=Math.sqrt(temp); j++) {
+				if(temp%j == 0) cnt++;
+				
+				if(cnt == 2) {
+					ans++;
+					break;
+				}
+			}
+		}
+		System.out.print(ans);
+		int bb = 4;
+		double aa = Math.sqrt(bb);
+		*/
+		
+		/*
+		int M = 64;
+		int N = 100;
+		int sum = 0;
+		int min = 0;
+
+		for(int i=M; i<=N; i++) {
+			if(isPrime(i)) {
+				sum += i;
+				if(min == 0) min = i;
+			}
+		}
+
+		System.out.println(sum);
+		System.out.println(min);
+		*/
+		
+		/*
+		int N = 72;
+
+
+		if(N != 1) {
+			int i = 2;
+			while(N%i == 0) {
+				System.out.println(i);
+				N = N / i;
+				i++;
+				
+				if(N%i == 0) break;
+			}
+		}
+		*/
+		
+        BufferedReader br   = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw   = new BufferedWriter(new OutputStreamWriter(System.out));
+        Queue<Integer> temp = new LinkedList<>();
+	}
+
+/*
+	public static boolean isPrime(int num) {
+		if(num == 1) return false;
+
+		for(int i=2; i<=Math.sqrt(num); i++) {
+			if(num%i == 0) return false;
+		}
+
+		return true;
+	}
+*/
+	
+}
