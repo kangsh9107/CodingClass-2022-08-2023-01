98bc3ad9d8b76636d1e28b6733afb3f0e405e2e9
kangsh9107 <68472349+kangsh9107@users.noreply.github.com>
2022-10-29 16:26:33 +0900

    2022-08-08~2022-10-29

---
+++ web-2022-08/src/main/java/standard/P221029.java
@@ -0,0 +1,42 @@
+package standard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P221029 {
+
+	public static void main(String[] args) throws IOException {
+		/*
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int r = Integer.parseInt(br.readLine());
+		final double pi = 3.14159265358979;
+		
+		System.out.printf("%.6f\n", pi*r*r);
+		System.out.printf("%d.000000", r*2*r);
+		*/
+		/*
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		while(true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			if(a == 0 && b == 0) break;
+			else if(b%a == 0) System.out.println("factor");
+			else if(a%b == 0) System.out.println("multiple");
+			else System.out.println("neither");
+		}
+		*/
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		
+	}
+
+}
