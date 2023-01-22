93c6c6130e05eca3cf0de162c831ccd1f657131f
kangsh9107 <kangsh9107@naver.com>
2023-01-23 02:20:48 +0900

    2022-08-08~2023-01-23

--- web-2022-08/src/main/java/standard/ThreeMusketeers.java
+++ web-2022-08/src/main/java/standard/ThreeMusketeers.java
@@ -1,19 +1,10 @@
 package standard;
 
 public class ThreeMusketeers {
+	int answer = 0;
 
 	public static void main(String[] args) {
 		ThreeMusketeers tm = new ThreeMusketeers();
-		
-		int n = 4;
-		int[] arr = {1, 2, 3, 4};
-		boolean[] visited = new boolean[n];
-		
-//		for(int i=1; i<=n; i++) {
-//			System.out.println("\n" + n + " 개 중에서 " + i + " 개 뽑기");
-//			combination(arr, visited, 0, n, i);
-//		}
-		combination(arr, visited, 0, n, 1);
 		
 		int[] number1 = {-2,3,0,2,-5};
 		System.out.println(tm.solution(number1));
@@ -26,33 +17,31 @@ public class ThreeMusketeers {
 	}
 	
 	int solution(int[] number) {
-		int answer = 0;
-		
-		
-		
+		answer = 0;
+		boolean[] visited = new boolean[number.length];		
+		combination(number, visited, 0, number.length, 3);
 		return answer;
 	}
 	
-	static void combination(int[] arr, boolean[] visited, int start, int n, int r) {
+	void combination(int[] number, boolean[] visited, int start, int n, int r) {
 		if(r == 0) {
-			print(arr, visited, n);
+			int sum = sum(number, visited, n);
+			if(sum == 0) answer++;
 			return;
 		}
-		
 		for(int i=start; i<n; i++) {
 			visited[i] = true;
-			combination(arr, visited, i+1, n, r-1);
+			combination(number, visited, i+1, n, r-1);
 			visited[i] = false;
 		}
 	}
 	
-	static void print(int[] arr, boolean[] visited, int n) {
+	int sum(int[] number, boolean[] visited, int n) {
+		int sum = 0;
 		for(int i=0; i<n; i++) {
-			if(visited[i]) {
-				System.out.print(arr[i] + " ");
-			}
+			if(visited[i]) sum += number[i];
 		}
-		System.out.println();
+		return sum;
 	}
 
 }
