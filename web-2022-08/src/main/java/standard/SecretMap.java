c6437447fe2cb8f765014ef02cd83f4bebbd56b4
kangsh9107 <kangsh9107@naver.com>
2023-01-28 23:01:52 +0900

    2022-08-08~2023-01-28

---
+++ web-2022-08/src/main/java/standard/SecretMap.java
@@ -0,0 +1,27 @@
+package standard;
+
+public class SecretMap {
+	
+	public void main(String[] args) {
+		SecretMap sm = new SecretMap();
+		
+		int n1 = 5;
+		int[] arr1 = {9, 20, 28, 18, 11};
+		int[] arr2 = {30, 1, 21, 17, 28};
+		System.out.println(sm.solution(n1, arr1, arr2));
+		
+		int n2 = 6;
+		int[] arr3 = {46, 33, 33, 22, 31, 50};
+		int[] arr4 = {27, 56, 19, 14, 14, 10};
+		System.out.println(sm.solution(n2, arr3, arr4));
+	}
+	
+	public String[] solution(int n, int[] arr1, int[] arr2) {
+		String[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
