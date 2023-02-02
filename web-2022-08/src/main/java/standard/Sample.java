1fb93c411de210b417524bb0c5e545df99e567ee
kangsh9107 <kangsh9107@naver.com>
2023-02-02 21:44:33 +0900

    study

--- web-2022-08/src/main/java/standard/Sample.java
+++
@@ -1,9 +0,0 @@
-package standard;
-
-public class Sample {
-	public static void main(String[] args) {
-		Calculator cal = new Calculator();
-		cal.add(10);
-		System.out.println(cal.getValue());
-	}
-}
